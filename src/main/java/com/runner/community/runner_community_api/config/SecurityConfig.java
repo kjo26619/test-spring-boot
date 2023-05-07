3be52e022d98f4db4fe4081311a69701fc674e8f
kjo26619 <kjo26619@gmail.com>
2023-05-07 18:37:25 +0900

    feat: Add Controller, Model, Repository, Serivce

---
+++ src/main/java/com/runner/community/runner_community_api/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.runner.community.runner_community_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.cors().and()
+                .csrf().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests().requestMatchers("/api/**").permitAll().and().build();
+    }
+
+}
